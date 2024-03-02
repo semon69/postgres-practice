7cad5bb02f70956503f090ca6baf02acc01fe1c8
unknown <smdemon3@gmail.com>
2024-03-02 13:07:34 +0600

    learn how to create functions in sql

---
+++ functionsProcedures.sql
@@ -0,0 +1,33 @@
+-- Active: 1709098687066@@127.0.0.1@5432@ph
+SELECT * FROM employees;
+
+CREATE Function emp_count()
+RETURNS INT
+LANGUAGE SQL
+as 
+$$
+    SELECT count(*) from employees;
+$$
+
+SELECT emp_count();
+
+
+CREATE Function delete_emp()
+RETURNS void
+LANGUAGE SQL
+as
+$$
+    DELETE FROM employees WHERE employee_id = 30
+$$
+
+CREATE Function delete_emp_with_p(p_emp_id  INT)
+RETURNS void
+LANGUAGE SQL
+as
+$$
+    DELETE FROM employees WHERE employee_id = p_emp_id
+$$
+
+SELECT delete_emp();
+
+SELECT delete_emp_with_p(29);
