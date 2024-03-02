ae26450799a04cb669ea128ea2a58f1d44a48ab6
unknown <smdemon3@gmail.com>
2024-03-02 11:35:32 +0600

    practiced more query

--- practice.sql
+++ practice.sql
@@ -11,7 +11,6 @@ CREATE TABLE departments (
 
 DROP Table departments;
 
-
 INSERT INTO
     departments (department_name)
 VALUES ('HR'),
@@ -120,23 +119,86 @@ VALUES (
         'Mia Roberts', 10, 70000.25, '2021-11-20'
     );
 
-
-
 SELECT * FROM departments;
 
 SELECT * FROM employees;
 
-
 -- retrive employees and department information
 -- both 2 are same
-SELECT * from employees as e
+SELECT *
+from employees as e
     INNER JOIN departments as d on e.department_id = d.department_id;
 
-SELECT * from employees
-    INNER JOIN departments USING(department_id);
+SELECT *
+from employees
+    INNER JOIN departments USING (department_id);
 
 -- show department name with average salary
-SELECT department_name, round(avg(salary)) from employees
-    JOIN departments USING(department_id)
-    GROUP BY department_name;
+SELECT department_name, round(avg(salary))
+from employees
+    JOIN departments USING (department_id)
+GROUP BY
+    department_name;
+
+-- count employees in each department
+SELECT department_name, count(*) as employees
+FROM employees
+    JOIN departments USING (department_id)
+GROUP BY
+    department_name;
+
+-- find department name with average salary
+SELECT department_name, avg(salary)
+FROM employees
+    JOIN departments USING (department_id)
+GROUP BY
+    department_name
+ORDER BY avg DESC
+LIMIT 1;
+
+--count employees hired in each year
+SELECT extract(
+        year
+        from hire_date
+    ) as hire_year, count(*) as hired
+from employees
+GROUP BY
+    hire_year;
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY, customer_id INT, order_date DATE, total_amount DECIMAL(10, 2)
+);
+
+INSERT INTO
+    orders (
+        customer_id, order_date, total_amount
+    )
+VALUES (1, '2022-01-05', 100.50),
+    (2, '2020-01-07', 200.75),
+    (1, '2022-01-08', 150.25),
+    (3, '2020-05-10', 300.00),
+    (2, '2022-01-15', 180.50),
+    (3, '2022-01-20', 220.25),
+    (1, '2022-01-25', 90.00),
+    (2, '2022-01-28', 120.75),
+    (3, '2021-02-01', 250.50),
+    (1, '2022-02-05', 180.25),
+    (4, '2023-02-05', 190.25);
+
+SELECT * from orders;
+
+
+--finding customer who have more than 2 orders and calculate the total amount spent by each of these customer
+SELECT customer_id, sum(total_amount) as total_amount, count(*)
+    from orders
+    GROUP BY customer_id
+    HAVING COUNT(*) > 2;
+
+
+
+-- Find the total amount of orders placed each month in the year 2022.
+SELECT extract( month from order_date ) as month, sum(total_amount)
+    FROM orders
+    WHERE extract( year from order_date) = 2022
+    GROUP BY month;
 
